8d7efcf1fd4020331d2ba110a531e9dcffb4725e
Agnieszka Polińska <agapoli@gmail.com>
2021-05-29 15:26:37 +0200

    Prepare second button

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import UsersList from './UsersList';
 import ButtonFetchUsers from './ButtonFetchUsers';
 import { Component } from 'react';
 
-const API = 'https://randomuser.me/api/?results=5';
+const API = 'https://randomuser.me/api/?results=';
 
 class App extends Component {
   state = {
@@ -11,7 +11,7 @@ class App extends Component {
   }
 
   handleDataFetch = () => {
-    fetch(API)
+    fetch(`${API}5`)
     .then(response => {
       if (response.ok) {
         return response;
@@ -27,13 +27,18 @@ class App extends Component {
     .catch(error => console.log(error))
   }
 
+  handleDataFetchOne = () => {
+    console.log('handleDataFetchOne')
+  }
+
   render() {
     const users = this.state.users;
     console.log(users)
     
     return (
       <div className="App">
-        <ButtonFetchUsers click={this.handleDataFetch}/>
+        <ButtonFetchUsers click={this.handleDataFetch} text="Pokaż 5 użytkowników"/>
+        <ButtonFetchUsers click={this.handleDataFetchOne} text="Dodaj użytkownika"/>
        { users ? <UsersList users={users}/> : users}
       </div>
     );
